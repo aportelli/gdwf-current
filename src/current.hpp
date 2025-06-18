9682e9d3d3bd52842176b5a9ed724c8b856651c9
Antonin Portelli <antonin.portelli@me.com>
2025-06-18 17:43:12 +0100

    first working version

---
+++ src/current.hpp
@@ -0,0 +1,101 @@
+#include <Grid/Grid.h>
+
+#define JWMultiply(qout, qin, sign)                      \
+  jtmp = Cshift(qin, mu, 1);                             \
+  action.multLinkField(Utmp, action.Umu, jtmp, mu);      \
+  jtmp = (sign) * (Utmp * ph - gmu * Utmp * ph);         \
+  jtmp = where((lcoor >= tmin), jtmp, zz);               \
+  jtmp = where((lcoor <= tmax), jtmp, zz);               \
+  qout = jtmp;                                           \
+  jtmp = qin * ph;                                       \
+  jtmp = Cshift(jtmp, mu, -1);                           \
+  action.multLinkField(Utmp, action.Umu, jtmp, mu + Nd); \
+  jtmp = -(sign) * (Utmp + gmu * Utmp);                  \
+  jtmp = where((lcoor >= tmin + tshift), jtmp, zz);      \
+  jtmp = where((lcoor <= tmax + tshift), jtmp, zz);      \
+  qout = qout + jtmp;
+
+#define Gs(s) (curr_type == Current::Axial) ? (((s) < Ls / 2) ? -1. : 1.) : 1.
+#define Pp(Q) (0.5 * (Q + g5 * Q))
+#define Pm(Q) (0.5 * (Q - g5 * Q))
+
+template <typename FImpl>
+using FermionField = typename FImpl::FermionField;
+
+template <typename FImpl>
+using PropagatorField = typename FImpl::PropagatorField;
+
+template <typename FImpl>
+using GaugeField = typename FImpl::GaugeField;
+
+template <typename FImpl>
+using ComplexField = typename FImpl::ComplexField;
+
+template <typename FImpl, template <typename> class Action>
+void seqConservedCurrent(PropagatorField<FImpl> &q_in, PropagatorField<FImpl> &q_out,
+                         PropagatorField<FImpl> &phys_src, Grid::Current curr_type,
+                         unsigned int mu, unsigned int tmin, unsigned int tmax,
+                         ComplexField<FImpl> &ph,
+                         Action<FImpl> &action) // Complex phase factor
+{
+  using namespace Grid;
+
+  int tshift = (mu == Grid::Nd - 1) ? 1 : 0;
+  int Ls = action.Ls;
+  auto UGrid = action.GaugeGrid();
+  auto FGrid = action.FermionGrid();
+  auto mass = action.Mass();
+
+  Gamma::Algebra Gmu[] = {Gamma::Algebra::GammaX, Gamma::Algebra::GammaY,
+                          Gamma::Algebra::GammaZ, Gamma::Algebra::GammaT};
+  Gamma gmu = Gamma(Gmu[mu]);
+  Gamma g5(Gamma::Algebra::Gamma5);
+
+  PropagatorField<FImpl> srcTerm(UGrid);
+  std::vector<PropagatorField<FImpl>> phi(Ls, UGrid);
+
+  PropagatorField<FImpl> tmp1(UGrid), jtmp(UGrid), tmp2(UGrid);
+  PropagatorField<FImpl> Utmp(UGrid);
+  PropagatorField<FImpl> zz(UGrid);
+  zz = 0.0;
+  LatticeInteger lcoor(UGrid);
+  LatticeCoordinate(lcoor, Nd - 1);
+
+  for (int s = 0; s < Ls; ++s)
+  {
+    ExtractSlice(phi[s], q_in, s, 0);
+  }
+  auto b = action.bs[0];
+  auto c = action.cs[0];
+  tmp1 = b * phi[0] + c * Pm(phi[1]) - mass * c * Pp(phi[Ls - 1]);
+  JWMultiply(tmp2, tmp1, Gs(0));
+  InsertSlice(tmp2, q_out, 0, 0);
+  for (int s = 1; s < Ls - 1; ++s)
+  {
+    b = action.bs[s];
+    c = action.cs[s];
+    tmp1 = c * Pp(phi[s - 1]) + b * phi[s] + c * Pm(phi[s + 1]);
+    JWMultiply(tmp2, tmp1, Gs(s));
+    InsertSlice(tmp2, q_out, s, 0);
+  }
+  b = action.bs[Ls - 1];
+  c = action.cs[Ls - 1];
+  tmp1 = -mass * c * Pm(phi[0]) + c * Pp(phi[Ls - 2]) + b * phi[Ls - 1];
+  JWMultiply(tmp2, tmp1, Gs(Ls - 1));
+  InsertSlice(tmp2, q_out, Ls - 1, 0);
+  tmp1 = action.cs[0] * Pp(phys_src);
+  JWMultiply(tmp2, tmp1, Gs(0));
+  ExtractSlice(srcTerm, q_out, 0, 0);
+  srcTerm += tmp2;
+  InsertSlice(srcTerm, q_out, 0, 0);
+  tmp1 = action.cs[Ls - 1] * Pm(phys_src);
+  JWMultiply(tmp2, tmp1, Gs(Ls - 1));
+  ExtractSlice(srcTerm, q_out, Ls - 1, 0);
+  srcTerm += tmp2;
+  InsertSlice(srcTerm, q_out, Ls - 1, 0);
+}
+
+#undef JWMultiply
+#undef Gs
+#undef Pp
+#undef Pm
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,134 @@
+#include "current.hpp"
 #include <Grid/Grid.h>
 
 using namespace Grid;
+
+template <typename Action>
+void computePropagator(PropagatorField<Action> &prop5, PropagatorField<Action> &prop4,
+                       PropagatorField<Action> &src, Action &action)
+{
+  ConjugateGradient<LatticeFermion> CG(1.0e-16, 100000);
+  SchurRedBlackDiagTwoSolve<LatticeFermion> schur(CG);
+  ZeroGuesser<LatticeFermion> zpg;
+
+  for (int s = 0; s < Nd; s++)
+  {
+    for (int c = 0; c < Nc; c++)
+    {
+      FermionField<Action> src4(src.Grid());
+      PropToFerm<Action>(src4, src, s, c);
+
+      FermionField<Action> src5(prop5.Grid());
+      action.ImportPhysicalFermionSource(src4, src5);
+
+      FermionField<Action> result5(prop5.Grid());
+      result5 = Zero();
+      schur(action, src5, result5, zpg);
+      std::cout << GridLogMessage << "spin " << s << " color " << c << " norm2(src5d) "
+                << norm2(src5) << " norm2(result5d) " << norm2(result5) << std::endl;
+      FermToProp<Action>(prop5, result5, s, c);
+
+      FermionField<Action> result4(prop4.Grid());
+      action.ExportPhysicalFermionSolution(result5, result4);
+      FermToProp<Action>(prop4, result4, s, c);
+    }
+  }
+}
+
+template <class Action>
+void TestRegression(Action &Ddwf, GaugeField<Action> &Umu, GridCartesian *FGrid,
+                    GridRedBlackCartesian *FrbGrid, GridCartesian *UGrid,
+                    GridRedBlackCartesian *UrbGrid, RealD mass, RealD M5,
+                    GridParallelRNG *RNG4, GridParallelRNG *RNG5)
+{
+  PropagatorField<Action> phys_src(UGrid);
+  PropagatorField<Action> seqsrc(FGrid), seqsrcalt(FGrid), diff(FGrid);
+  PropagatorField<Action> prop5(FGrid);
+  PropagatorField<Action> prop4(UGrid);
+
+  random(*RNG4, phys_src);
+  random(*RNG5, prop5);
+
+  auto curr = Current::Vector;
+  const int mu_J = 0;
+  const int t_J = 0;
+  double time;
+  ComplexField<Action> ph(UGrid);
+  ph = 1.0;
+
+  time = -usecond();
+  Ddwf.SeqConservedCurrent(prop5, seqsrc, phys_src, curr, mu_J, t_J, t_J, ph);
+  time += usecond();
+  std::cout << GridLogMessage << "Grid implementation -- norm2= " << norm2(seqsrc)
+            << " -- time= " << time / 1.0e6 << "s" << std::endl;
+  time = -usecond();
+  seqConservedCurrent(prop5, seqsrcalt, phys_src, curr, mu_J, t_J, t_J, ph, Ddwf);
+  time += usecond();
+  std::cout << GridLogMessage << "this implementation -- norm2= " << norm2(seqsrcalt)
+            << " -- time= " << time / 1.0e6 << "s" << std::endl;
+  diff = seqsrcalt - seqsrc;
+  std::cout << GridLogMessage << "rel diff norm2= " << norm2(diff) / norm2(seqsrc)
+            << std::endl;
+}
 
 int main(int argc, char **argv)
 {
   Grid_init(&argc, &argv);
 
-  std::cout << GridLogMessage << "Grid initialized successfully." << std::endl;
+  int threads = GridThread::GetThreads();
+  std::cout << GridLogMessage << "Grid is setup to use " << threads << " threads"
+            << std::endl;
 
-  // Your application logic here
+  const int Ls = 10;
+  std::vector<ComplexD> omegas;
+
+  omegas.push_back(std::complex<double>(1.45806438985048, -0));
+  omegas.push_back(std::complex<double>(1.18231318389348, -0));
+  omegas.push_back(std::complex<double>(0.830951166685955, -0));
+  omegas.push_back(std::complex<double>(0.542352409156791, -0));
+  omegas.push_back(std::complex<double>(0.341985020453729, -0));
+  omegas.push_back(std::complex<double>(0.21137902619029, -0));
+  omegas.push_back(std::complex<double>(0.126074299502912, -0));
+  omegas.push_back(std::complex<double>(0.0990136651962626, -0));
+  omegas.push_back(std::complex<double>(0.0686324988446592, 0.0550658530827402));
+  omegas.push_back(std::complex<double>(0.0686324988446592, -0.0550658530827402));
+
+  GridCartesian *UGrid = SpaceTimeGrid::makeFourDimGrid(
+      GridDefaultLatt(), GridDefaultSimd(Nd, vComplex::Nsimd()), GridDefaultMpi());
+  GridRedBlackCartesian *UrbGrid = SpaceTimeGrid::makeFourDimRedBlackGrid(UGrid);
+  GridCartesian *FGrid = SpaceTimeGrid::makeFiveDimGrid(Ls, UGrid);
+  GridRedBlackCartesian *FrbGrid = SpaceTimeGrid::makeFiveDimRedBlackGrid(Ls, UGrid);
+
+  std::vector<int> seeds5({5, 6, 7, 8});
+  GridParallelRNG RNG5(FGrid);
+  RNG5.SeedFixedIntegers(seeds5);
+  GridParallelRNG RNG4(UGrid);
+  std::vector<int> seeds4({1, 2, 3, 4});
+  RNG4.SeedFixedIntegers(seeds4);
+
+  LatticeGaugeField Umu(UGrid);
+  SU<Nc>::HotConfiguration(RNG4, Umu);
+
+  RealD mass = 0.3;
+  RealD M5 = 1.0;
+
+  std::cout << GridLogMessage << "-- DWF regression test" << std::endl;
+  DomainWallFermionD Ddwf(Umu, *FGrid, *FrbGrid, *UGrid, *UrbGrid, mass, M5);
+  TestRegression<DomainWallFermionD>(Ddwf, Umu, FGrid, FrbGrid, UGrid, UrbGrid, mass, M5,
+                                     &RNG4, &RNG5);
+
+  std::cout << GridLogMessage << "-- Moebius DWF regression test" << std::endl;
+  ScaledShamirFermionD Dsham(Umu, *FGrid, *FrbGrid, *UGrid, *UrbGrid, mass, M5, 2.0);
+  TestRegression<ScaledShamirFermionD>(Dsham, Umu, FGrid, FrbGrid, UGrid, UrbGrid, mass,
+                                       M5, &RNG4, &RNG5);
+
+  std::cout << GridLogMessage << "-- z-Moebius DWF regression test" << std::endl;
+  ZMobiusFermionD ZDmob(Umu, *FGrid, *FrbGrid, *UGrid, *UrbGrid, mass, M5, omegas, 1.0,
+                        0.0);
+  TestRegression<ZMobiusFermionD>(ZDmob, Umu, FGrid, FrbGrid, UGrid, UrbGrid, mass, M5,
+                                  &RNG4, &RNG5);
 
   Grid_finalize();
-
-  std::cout << GridLogMessage << "Grid finalized successfully." << std::endl;
 
   return EXIT_SUCCESS;
 }
